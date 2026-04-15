6d219501e40e26591718974d01f4cf868a9d539e
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-15 18:39:50 +0530

    find the value of the following expression: int x = 7*49/7 + 35/7;

---
+++ Practice2_5.java
@@ -0,0 +1,7 @@
+// find the value of the following expression: int x = 7*49/7 + 35/7;
+public class Practice2_5 {
+    public static void main(String[] args) {
+        int x = 7 * 49 / 7 + 35 / 7;
+        System.out.println("The value of the expression is: " + x);
+    }
+}
