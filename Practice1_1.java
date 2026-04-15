f634780a140e864c7e06863dea266fbb9513ebd0
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-15 16:41:32 +0530

    First practice question in which we Write a Java program that declares three integer variables, assigns them values, calculates their sum, and prints the result.

---
+++ Practice1_1.java
@@ -0,0 +1,12 @@
+// Write a Java program that declares three integer variables, assigns them values, calculates their sum, and prints the result. 
+
+public class Practice1_1 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = 6;
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+
+}
