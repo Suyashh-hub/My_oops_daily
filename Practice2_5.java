65fe5fd2c8160f39860bfee5cb75963b429c7a89
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-16 23:37:37 +0530

    //`Write a program to convert a string to lowercase.

--- Practice2_5.java
+++ Practice2_5.java
@@ -1,6 +1,7 @@
 // find the value of the following expression: int x = 7*49/7 + 35/7;
 public class Practice2_5 {
     public static void main(String[] args) {
+        int a = 7;
         int x = 7 * 49 / 7 + 35 / 7;
         System.out.println("The value of the expression is: " + x);
     }
---
+++ Practice3_1.java
@@ -0,0 +1,10 @@
+//`Write a program to convert a string to lowercase.
+public class Practice3_1 {
+    public static void main(String[] args) {
+        String str = "HELLO WORLD";
+        String lowerCaseStr = str.toLowerCase();
+        System.out.println("Original String: " + str);
+        System.out.println("Lowercase String: " + lowerCaseStr);
+    }
+
+}
