a440b34a2cd2882e4675d5b0a9acde9a1365b99a
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-16 23:55:24 +0530

    Write a java program to detect double and triple spaces in a string by user input.

---
+++ Practice3_4.java
@@ -0,0 +1,26 @@
+
+//Write a java program to detect double and triple spaces in a string by user input.
+import java.util.Scanner;
+
+public class Practice3_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        boolean hasDoubleSpaces = inputString.contains("  ");
+        boolean hasTripleSpaces = inputString.contains("   ");
+
+        if (hasDoubleSpaces) {
+            System.out.println("The string contains double spaces.");
+        } else {
+            System.out.println("The string does not contain double spaces.");
+        }
+
+        if (hasTripleSpaces) {
+            System.out.println("The string contains triple spaces.");
+        } else {
+            System.out.println("The string does not contain triple spaces.");
+        }
+    }
+
+}
