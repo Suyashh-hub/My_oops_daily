dd77dac2313acd5cc27c55fe7b1fbdb0045ffb32
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-16 23:58:42 +0530

    Write a program to format the following letter using escape sequence characters: // Letter template: "Dear Suyash, Thanks a lot"

---
+++ Practice3_5.java
@@ -0,0 +1,8 @@
+// Write a program to format the following letter using escape sequence characters:
+// Letter template: "Dear Suyash, Thanks a lot"
+public class Practice3_5 {
+    public static void main(String[] args) {
+        String letterTemplate = "Dear Suyash,\nThanks a lot";
+        System.out.println(letterTemplate);
+    }
+}
