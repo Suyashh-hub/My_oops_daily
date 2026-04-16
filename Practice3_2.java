3ca82b6abcd09345410e18a3dd3dd236997035e9
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-16 23:39:47 +0530

    Write a java program to replace spaces with underscores in a string.

---
+++ Practice3_2.java
@@ -0,0 +1,9 @@
+// Write a java program to replace spaces with underscores in a string.
+public class Practice3_2 {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String replacedStr = str.replace(" ", "_");
+        System.out.println("Original String: " + str);
+        System.out.println("String with underscores: " + replacedStr);
+    }
+}
