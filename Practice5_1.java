df4a0473acf2dc66eb5060a53c48cf5e3400c64d
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-19 23:57:55 +0530

    Write a PRogram to print the following pattern: // * * * * // * * * // * * // *

---
+++ Practice5_1.java
@@ -0,0 +1,16 @@
+// Write a PRogram to print the following pattern:
+// * * * *  
+// * * *
+// * *
+// *
+public class Practice5_1 {
+    public static void main(String[] args){
+        int n=4;
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;;j++){
+                System.out.print("* ");
+            }
+        }
+    }
+    
+}
