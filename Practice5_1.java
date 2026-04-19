6da5ff681cf81748ee18804f7ac7e691b48e6102
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-20 00:01:46 +0530

    Write a PRogram to print the following pattern: // * * * * // * * * // * * // *

--- Practice5_1.java
+++ Practice5_1.java
@@ -4,13 +4,13 @@
 // * *
 // *
 public class Practice5_1 {
-    public static void main(String[] args){
-        int n=4;
-        for(int i=n;i>0;i--){
-            for(int j=0;j<i;;j++){
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("* ");
             }
+            System.out.println(); // Moves to the next line after each row
         }
     }
-    
 }
