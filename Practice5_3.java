9d13fe0841e5c5cc1d07a43b67a4b2fdfb54c37b
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-20 00:24:19 +0530

    Write a java progra to print muultiplication table of a number entered by the user.

---
+++ Practice5_3.java
@@ -0,0 +1,18 @@
+
+// Write a java progra to print muultiplication table of a number entered by the user.
+import java.util.Scanner;
+
+public class Practice5_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Multiplication table of " + num + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+        sc.close();
+
+    }
+
+}
