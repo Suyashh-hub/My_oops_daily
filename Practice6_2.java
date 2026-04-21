df5cf6afdda45d5259418119bbbeda080a668f4a
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-21 23:39:28 +0530

    Write a Program to find out whether a given integer is present in an array or not.

--- Practice6_2.java
+++ Practice6_2.java
@@ -1,9 +1,13 @@
+
 // Write a Program to find out whether a given integer is present in an array or not.
+import java.util.Scanner;
 
 public class Practice6_2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int[] arr = { 1, 2, 3, 4, 5 };
-        int num = 3;
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
         boolean found = false;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == num) {
