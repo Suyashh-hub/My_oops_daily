f8dc6f851cd137c3bd645b5d613e2b86201a158e
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-21 23:22:03 +0530

    Create an array of 5 float values and calculate their sum.

--- Practice6_1.java
+++ Practice6_1.java
@@ -1,23 +1,19 @@
-// create a class employee with following properties
-//salary,get salary, name,get name,set name
-class Employee {
-    private double salary;
-    private String name;
 
-    public Employee(double salary, String name) {
-        this.salary = salary;
-        this.name = name;
-    }
+//Create an array of 5 float values and calculate their sum.
+import java.util.Scanner;
 
-    public double getSalary() {
-        return salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+public class Practice6_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float[] arr = new float[5];
+        System.out.println("Enter 5 float values: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextFloat();
+        }
+        float sum = 0;
+        for (float f : arr) {
+            sum += f;
+        }
+        System.out.println("Sum of the float values is: " + sum);
     }
 }
