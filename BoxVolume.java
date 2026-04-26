d62a18897a280ee50bc1dbc45597124008c9577d
Suyashh-hub <suyashnandeshwar8@gmail.com>
2026-04-26 18:11:25 +0530

    Program that declares two box objects and assigns values to their length, width, and height. Then, calculate and display the volume of each box.

--- BoxVolume.java
+++ BoxVolume.java
@@ -17,16 +17,18 @@ public class BoxVolume {
         Box box1 = new Box();
         Box box2 = new Box();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length, width and height pf the Box1: ");
+        System.out.println("Enter the length, width and height of the Box1: ");
         box1.length = sc.nextDouble();
         box1.width = sc.nextDouble();
         box1.height = sc.nextDouble();
-        System.out.println("Enter the length, width and height pf the Box2: ");
+        System.out.println("Enter the length, width and height of the Box2: ");
         box2.length = sc.nextDouble();
         box2.width = sc.nextDouble();
         box2.height = sc.nextDouble();
         System.out.println("Volume of Box1: " + box1.volume());
         System.out.println("Volume of Box2: " + box2.volume());
+        sc.close();
 
     }
+
 }
